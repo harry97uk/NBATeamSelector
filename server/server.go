b98abb8382303bcb8cae2f2039f5af460609fc77
Harry Gardiner <harry97uk@hotmail.co.uk>
2023-06-30 15:57:11 +0100

    added api list and find closest ua

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -17,7 +18,12 @@ func enableCors(w *http.ResponseWriter) {
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/players", handlePlayersApiCall).Methods("GET")
+	router.HandleFunc("/api/continents", handleContinentsApiCall).Methods("GET")
+	router.HandleFunc("/api/countries", handleCountriesApiCall).Methods("GET")
+	router.HandleFunc("/api/areas", handleAdminDivsApiCall).Methods("GET")
+	router.HandleFunc("/api/cities", handleCitiesApiCall).Methods("GET")
+	router.HandleFunc("/api/city", handleCityApiCall).Methods("GET")
+	router.HandleFunc("/api/uas", handleUasApiCall).Methods("GET")
 	router.HandleFunc("/signin", func(w http.ResponseWriter, r *http.Request) {}).Methods("POST")
 	router.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) {}).Methods("GET")
 
@@ -41,21 +47,216 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func handlePlayersApiCall(w http.ResponseWriter, r *http.Request) {
+func handleContinentsApiCall(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 
-	params := r.URL.Query()
+	url := "https://api.teleport.org/api/continents/"
 
-	page := params.Get("page")
-	per_page := params.Get("per_page")
-	player_id := params.Get("player_id")
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	w.Write(body)
+}
+
+func handleCountriesApiCall(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	continentID := r.URL.Query().Get("continentID")
 
 	url := ""
 
-	if player_id == "" {
-		url = "https://www.balldontlie.io/api/v1/players?page=" + page + "?per_page=" + per_page
+	if continentID == "" {
+		url = "https://api.teleport.org/api/countries/"
 	} else {
-		url = "https://www.balldontlie.io/api/v1/players/" + player_id
+		url = "https://api.teleport.org/api/continents/" + continentID + "/countries/"
+	}
+
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	w.Write(body)
+}
+
+func handleAdminDivsApiCall(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	countryID := r.URL.Query().Get("countryID")
+
+	url := ""
+
+	if countryID == "" {
+		log.Fatal("Invalid countryID")
+	} else {
+		url = "https://api.teleport.org/api/countries/" + countryID + "/admin1_divisions/"
+	}
+
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	w.Write(body)
+}
+
+func handleCitiesApiCall(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	areaID := r.URL.Query().Get("areaID")
+	countryID := r.URL.Query().Get("countryID")
+
+	url := ""
+
+	if countryID != "" && areaID != "" {
+		url = "https://api.teleport.org/api/countries/" + countryID + "/admin1_divisions/" + areaID + "/cities/"
+	} else {
+		url = "https://api.teleport.org/api/cities/"
+	}
+
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	w.Write(body)
+}
+
+func handleCityApiCall(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	cityID := r.URL.Query().Get("cityID")
+
+	url := ""
+
+	if cityID != "" {
+		url = "https://api.teleport.org/api/cities/" + cityID
+	} else {
+		log.Fatal("Invalid City ID")
+	}
+
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	w.Write(body)
+}
+
+func handleUasApiCall(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+
+	uaID := r.URL.Query().Get("uaID")
+
+	url := ""
+
+	if uaID != "" {
+		fmt.Println(uaID)
+		url = "https://api.teleport.org/api/urban_areas/" + uaID + "/"
+	} else {
+		url = "https://api.teleport.org/api/urban_areas/"
 	}
 
 	spaceClient := http.Client{
